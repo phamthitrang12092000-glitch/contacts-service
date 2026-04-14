b486764865c095852c1f9bc808618b3af37d614b
Trang <phamthitrang12092000@gmail.com>
2026-04-14 07:42:09 +0000

    Initial commit - contacts service

---
+++ contacts-json.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import pymysql
+import json
+
+print("Content-Type: application/json; charset=utf-8\n")
+
+conn = pymysql.connect(
+    host="localhost",
+    user="webuser",
+    password="1234",
+    database="contactsdb",
+    charset="utf8"
+)
+cur = conn.cursor()
+cur.execute("SELECT name, phone FROM contacts")
+
+rows = cur.fetchall()
+data = [{"name": name, "telephone": phone} for name, phone in rows]
+
+result = {
+    "ok": True,
+    "count": len(data),
+    "data": data
+}
+
+print(json.dumps(result, ensure_ascii=False))
+
+cur.close()
+conn.close()
